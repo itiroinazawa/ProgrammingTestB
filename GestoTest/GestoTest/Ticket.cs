f8c3543c1f6dea217ba854e95dde4b723758ebbb
Itiro Inazawa <itiro.inazawa@hotmail.com>
2018-09-09 23:12:09 -0300

    Upload Aplicacao

---
+++ GestoTest/GestoTest/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoTest
+{
+    public class Ticket
+    {
+        public int Mes { get; set; }
+        public int Quantidade { get; set; }    
+    }
+}
